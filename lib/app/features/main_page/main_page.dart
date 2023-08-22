e84739ed3b1e6977b757ad5370f57b8f20b46d12
macieksulikowski <suleymaniana99@gmail.com>
2023-08-22 23:23:38 +0200

    Move class MainPage to main_page.dart from main.dart

---
+++ lib/app/features/main_page/main_page.dart
@@ -0,0 +1,60 @@
+import 'package:day_quote/app/features/quotes/quotes_page.dart';
+import 'package:day_quote/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+class MainPage extends StatelessWidget {
+  const MainPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.purpleAccent,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                'DayQuote',
+                style: GoogleFonts.orienta(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(height: 50),
+            InkWell(
+              onTap: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (_) => const QuotePage()));
+              },
+              child: const CircleAvatar(
+                radius: 100,
+                backgroundColor: Colors.black,
+                child: Icon(
+                  Icons.question_mark,
+                  size: 150,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            const SizedBox(height: 80),
+            Text(
+              'Kliknij wyżej żeby wylosować cytat!',
+              style: GoogleFonts.orienta(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -24,61 +24,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MainPage(),
+      home: const AuthGate(),
     );
   }
 }
 
-class MainPage extends StatelessWidget {
-  const MainPage({
-    super.key,
-  });
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.purpleAccent,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Text(
-                'DayQuote',
-                style: GoogleFonts.orienta(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            const SizedBox(height: 50),
-            InkWell(
-              onTap: () {
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (_) => const QuotePage()));
-              },
-              child: const CircleAvatar(
-                radius: 100,
-                backgroundColor: Colors.black,
-                child: Icon(
-                  Icons.question_mark,
-                  size: 150,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-            const SizedBox(height: 80),
-            Text(
-              'Kliknij wyżej żeby wylosować cytat!',
-              style: GoogleFonts.orienta(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
