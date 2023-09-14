a9c77abcdd8bcfe8b757cd25d5961f993ea8d9c4
macieksulikowski <suleymaniana99@gmail.com>
2023-09-14 11:52:54 +0200

    Implement freezed in quotes_state.dart

--- lib/app/features/quotes/cubit/quotes_cubit.dart
+++ lib/app/features/quotes/cubit/quotes_cubit.dart
@@ -2,18 +2,18 @@ import 'package:bloc/bloc.dart';
 import 'package:day_quote/app/core/enums.dart';
 import 'package:day_quote/app/domain/models/quotes_model.dart';
 import 'package:day_quote/app/domain/repositories/quotes_repository.dart';
+import 'package:day_quote/app/features/quotes/cubit/quotes_state.dart';
 import 'package:meta/meta.dart';
 
-part 'quotes_state.dart';
 
 class QuotesCubit extends Cubit<QuotesState> {
-  QuotesCubit(this._quotesRepository) : super(const QuotesState());
+  QuotesCubit(this._quotesRepository) : super( QuotesState());
 
   final QuotesRepository _quotesRepository;
 
   Future<void> getQuotes() async {
     emit(
-      const QuotesState(
+       QuotesState(
         status: Status.loading,
       ),
     );
--- lib/app/features/quotes/cubit/quotes_state.dart
+++ lib/app/features/quotes/cubit/quotes_state.dart
@@ -1,13 +1,27 @@
-part of 'quotes_cubit.dart';
+import 'package:day_quote/app/core/enums.dart';
+import 'package:day_quote/app/domain/models/quotes_model.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-@immutable
-class QuotesState {
-  const QuotesState({
-    this.quotesModel = const [],
-    this.status = Status.initial,
-    this.errorMessage,
-  });
-  final List<QuotesModel> quotesModel;
-  final Status status;
-  final String? errorMessage;
+part 'quotes_state.freezed.dart';
+
+@freezed
+class QuotesState with _$QuotesState {
+  factory QuotesState({
+    @Default([]) List<QuotesModel> quotesModel,
+    @Default(Status.initial) Status status,
+    String? errorMessage,
+  }) = _QuotesState;
 }
+
+
+// @immutable
+// class QuotesState {
+//   const QuotesState({
+//     this.quotesModel = const [],
+//     this.status = Status.initial,
+//     this.errorMessage,
+//   });
+//   final List<QuotesModel> quotesModel;
+//   final Status status;
+//   final String? errorMessage;
+// }
